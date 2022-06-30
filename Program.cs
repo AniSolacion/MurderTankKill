9b2c2cd6d2eb38ba918585d642429b78eef48a5c
JosephCheney <che20001@byui.edu>
2022-06-30 16:52:04 -0600

    added constants and program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Murdertankkill.Game.Casting;
+using Murdertankkill.Game.Directing;
+using Murdertankkill.Game.Scripting;
+using Murdertankkill.Game.Services;
+using Murdertankkill.Game;
+
+
+namespace Murdertankkill
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            // create the cast
+            Cast cast = new Cast();
+            cast.AddActor("bike", new Bike(Constants.BIKESTART, Constants.RED));
+            cast.AddActor("bike2", new Bike(Constants.BIKE2START, Constants.YELLOW));
+            cast.AddActor("score", new Score());
+
+            // create the services
+            KeyboardService keyboardService = new KeyboardService();
+            VideoService videoService = new VideoService(false);
+           
+            // create the script
+            Script script = new Script();
+            script.AddAction("input", new ControlActorsAction(keyboardService));
+            script.AddAction("update", new MoveActorsAction());
+            script.AddAction("update", new HandleCollisionsAction());
+            script.AddAction("output", new DrawActorsAction(videoService));
+
+            // start the game
+            Director director = new Director(videoService);
+            director.StartGame(cast, script);
+        }
+    }
+}
+
