9cd804b5a2dc43298a5ecc85f1a07c0cf2f40fdd
MikeChild <mcechild@gmail.com>
2022-07-07 17:00:03 -0600

    tank movement

--- Game/Scripting/HandleTankMovementAction.cs
+++ Game/Scripting/HandleTankMovementAction.cs
@@ -26,11 +26,44 @@ namespace asteroid.script {
             
             // Grab the tank from the cast
             this.tank = cast.GetFirstActor("tank");
-            
+
             Actor tank1 = cast.GetFirstActor("tank1");
+            Actor tank2 = cast.GetFirstActor("tank1");
+
 
             // Only move if ship is not null
-            if (this.tank != null) {
+            if (this.tank1 != null) {
+                
+                // Get the keysState from the keyboardService
+                Dictionary<int, bool> keysState = keyboardService.GetKeysState(this.keysOfInterest);
+                
+                // Change the velocity to the appropriate value and let MoveActorsAction handle the
+                // actual movement
+                if (keysState[Keys.LEFT]) {
+                    this.tank.SetVx(-this.tankMovementVel);
+                }
+                if (keysState[Keys.RIGHT]) {
+                    this.tank.SetVx(this.tankMovementVel);
+                }
+                if (keysState[Keys.DOWN]) {
+                    this.tank.SetVy(this.tankMovementVel);
+                }
+                if (keysState[Keys.UP]) {
+                    this.tank.SetVy(-this.tankMovementVel);
+                }
+
+                // If none of the LEFT or RIGHT keys are down, x-velocity is 0
+                if (!(keysState[Keys.LEFT] || keysState[Keys.RIGHT])) {
+                    this.tank.SetVx(0);
+                }
+
+                // If none of the UP or DOWN keys are down, y-velocity is 0
+                if (!(keysState[Keys.UP] || keysState[Keys.DOWN])) {
+                    this.tank.SetVy(0);
+                }
+            }
+
+            if (this.tank2 != null) {
                 
                 // Get the keysState from the keyboardService
                 Dictionary<int, bool> keysState = keyboardService.GetKeysState(this.keysOfInterest);
