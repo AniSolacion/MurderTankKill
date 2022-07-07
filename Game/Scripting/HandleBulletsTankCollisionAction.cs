1e6cefd5aca4e4a64892ae82f925056a3555b796
Logan Huston <shadows7590@gmail.com>
2022-07-07 16:31:27 -0600

    Took out extra code

--- Game/Scripting/HandleBulletsTankCollisionAction.cs
+++ Game/Scripting/HandleBulletsTankCollisionAction.cs
@@ -11,22 +11,16 @@ namespace asteroid.script {
         
         // Member Variables
         RaylibPhysicsService physicsService;
-        RaylibAudioService audioService;
-        private PlayerScore? score;
         private List<Actor> bullets;
 
 
         // Constructor
         public HandleBulletsAsteroidsCollisionAction(int priority, RaylibPhysicsService physicsService, RaylibAudioService audioService) : base(priority) {
-            this.score = null;
             this.physicsService = physicsService;
-            this.audioService = audioService;
             this.bullets = new List<Actor>();
         }
 
         public override void execute(Cast cast, Script script, Clock clock, Callback callback) {
-            // Grab the score from the cast
-            this.score = (PlayerScore?) cast.GetFirstActor("score");
 
             // First, get a list of bullets out of the cast
             bullets = cast.GetActors("bullets");
@@ -37,15 +31,10 @@ namespace asteroid.script {
                 if (collidedBullet != null) {
                     cast.RemoveActor("bullets", collidedBullet);
                     asteroid.TakeDamage(1);
-                    this.audioService.PlaySound("asteroid/assets/sound/rock_cracking.wav", (float) 0.1);
 
-                    // Destroy asteroid if its health is 0 and give player a score
+                    // Destroy asteroid if its health is 0
                     if (asteroid.GetHP() <= 0) {
                         cast.RemoveActor("asteroids", asteroid);
-                        if (this.score != null) {
-                            this.score.AddScore(asteroid.GetPoints());
-                        }
-                        this.audioService.PlaySound("asteroid/assets/sound/explosion-01.wav", (float) 0.1);
                     }
                 }
             }
