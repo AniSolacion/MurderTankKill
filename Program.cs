e3bfab4acaa2063628056a7790374a6687e0e011
JosephCheney <che20001@byui.edu>
2022-07-08 16:57:59 -0600

    changed start location

--- Program.cs
+++ Program.cs
@@ -56,8 +56,8 @@ namespace asteroid
             // Create the player
             //Ship ship = new Ship("./asteroid/assets/spaceship/spaceship_yellow.png", 70, 50, W_SIZE.Item1/2, mothership.GetTopLeft().Item2 - 40, 0, 0, 180);
 
-            Tank tank1 = new Tank("", TANK_WIDTH, TANK_LENGTH, 200, 200, 0, 0, 0, 0);
-            Tank tank2 = new Tank("", TANK_WIDTH, TANK_LENGTH, 400, 400, 0, 0, 0, 0);
+            Tank tank1 = new Tank("", TANK_WIDTH, TANK_LENGTH, 100, 100, 0, 0, 0, 0);
+            Tank tank2 = new Tank("", TANK_WIDTH, TANK_LENGTH, 800, 600, 0, 0, 0, 0);
 
             // Give actors to cast
             //cast.AddActor("ship", ship);
